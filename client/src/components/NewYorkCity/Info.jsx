24520e5ba8fd063cb14cf6f8ae9cea6fe3c703e3
cherrih <cherri.hartigan@gmail.com>
2019-01-28 12:24:44 -0800

    Render info about NYC

--- client/src/components/NewYorkCity/Info.jsx
+++ client/src/components/NewYorkCity/Info.jsx
@@ -8,8 +8,11 @@ const Info = (props) => {
         <div className="city-header">NYC</div>
         <div className="city-country">United States</div>
         <div className="city-filters">
-          <select className="city-select">
+          <select className="city-select" id="city-select-city">
             <option value="NYC">NYC</option>
+            <option value="San Francisco">San Francisco</option>
+            <option value="Wellington">Wellington</option>
+            <option value="Quito">Quito</option>
           </select>
           <select className="city-select" id="city-select-identity">
             <option value="LGBTQIA+">LGBTQIA+</option>
@@ -24,7 +27,7 @@ const Info = (props) => {
             <option value="Asexual">Asexual</option>
             <option value="Female-Owned">Female-Owned</option>
           </select>
-          <select className="city-select" id="">
+          <select className="city-select" id="city-select-category">
             <option value="All">All</option>
             <option value="Bars">Bars</option>
             <option value="Restaurants">Restaurants</option>
@@ -33,6 +36,27 @@ const Info = (props) => {
           </select>
         </div>
       </div>
+      <div className="city-facts">
+        <div id="queerdar" className="city-facts-item">
+          <img src="https://s3-us-west-1.amazonaws.com/queertrip/Rainbow.png"/>
+          <div id="city-queerdar">queerdar</div>
+          <div>100%</div>
+        </div>
+        <div id="weather" className="city-facts-item">
+          <img src="https://s3-us-west-1.amazonaws.com/queertrip/cloud.png"/>
+          <div>weather -10C</div>
+          <div>snowy AF</div>
+        </div>
+        <div id="cost" className="city-facts-item">
+          <img src="https://s3-us-west-1.amazonaws.com/queertrip/Money.png"/>
+          <div>cost per day $140 USD</div>
+          <div>expensive AF</div>
+        </div>
+      </div>
+      <div className="city-description">
+        Neutra normcore vice, schlitz scenester affogato duis consectetur la croix irure tousled swag tofu. Yuccie tumeric slow-carb, dolore poutine retro DIY in. Schlitz hammock mollit trust fund disrupt quinoa mumblecore live-edge tumblr artisan deserunt sunt. Man braid bicycle rights leggings commodo nisi, flannel skateboard deep v williamsburg aliquip vice franzen humblebrag hot chicken. Mlkshk la croix crucifix chillwave, vaporware letterpress wolf taiyaki meggings elit 8-bit craft beer raw denim chartreuse excepteur.
+      </div>
+      <div className="city-next-button"></div>
     </div>
   )
 };
