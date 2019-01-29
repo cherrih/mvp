14c6c3fd58af8e22418e64ec376bfd98bbe7a326
cherrih <cherri.hartigan@gmail.com>
2019-01-29 11:01:56 -0800

    Render images on page

--- client/src/components/Home/Details/Details.jsx
+++ client/src/components/Home/Details/Details.jsx
@@ -6,9 +6,20 @@ import CityList from './CityList/CityList.jsx';
 const Details = (props) => {
   return (
     <div id="details-body">
-      <div className="details-heading">  Helping queerdos <br/> travel queerer. </div>
-      <Search handleSubmit={props.handleSubmit}/>
+      <div id="details-top">
+        <div className="details-heading">  Helping queerdos <br/> travel queerer. </div>
+        <Search handleSubmit={props.handleSubmit}/>
+        <img id="contact" src="https://s3-us-west-1.amazonaws.com/queertrip/contact.png"/>
+        <img id="collabo" src="https://s3-us-west-1.amazonaws.com/queertrip/collabo.png"/>
+        <img id="why" src="https://s3-us-west-1.amazonaws.com/queertrip/why.png"/>
+      </div>
       <CityList cities={props.cities} handleCityClick={props.handleCityClick}/>
+      <div className="orgs">
+        <div>
+          Here's a short list of organizations doing work to support queer and trans people, with special consideration for: youth empowerment, homelessness, mental health, incarceration, and structural racism. We are not partners with any of them, we just believe in their work.
+        </div>
+        <img src="https://s3-us-west-1.amazonaws.com/queertrip/Rainbow.png"/>
+      </div>
     </div>
   )
 };
